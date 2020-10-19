63482f551fd8ae0d7a36b565b6b026bd3aacf816
Henrik Bulskov <bulskov@ruc.dk>
2020-10-19 23:25:29 +0200

    Anonymous Types

---
+++ 5-AnonymousTypes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _5_AnonymousTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var s = new {Name = "Peter", Age = 23};
+
+            var x = CreateAnonType();
+
+
+            Console.WriteLine(x);
+
+
+        }
+
+        static object CreateAnonType()
+        {
+            return new {Id = 2, Phone = 123456};
+        }
+    }
+}
