79ec481e9b24e7bae05a5f6168b55a765df92ec8
Henrik Bulskov <bulskov@ruc.dk>
2020-10-19 23:25:46 +0200

    Tuples

---
+++ 6-Tuples/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _6_Tuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            
+            var res = MyMethod();
+
+            Print(res);
+
+            //Console.WriteLine($"Name={res.Name}, Number={res.Number}");
+        }
+
+        static (string Name, int Number) MyMethod()
+        {
+            return ("Hello", 123);
+        }
+
+        static void Print((string Name, int Number) x)
+        {
+            Console.WriteLine($"Name={x.Name}, Number={x.Number}");
+        }
+    }
+}
